4b1702203917feb7726d748a3dff9f234c86ef2d
Garrett <garres4@vt.edu>
2021-10-14 16:03:16 -0600

    commit tree 9a7e9ae38059b0d34f3f17cc83a1f72f953de231

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function batteries(runningTotal, currentValue) {
+    return runningTotal + currentValue;
+}
+let totalBatteries = batteryBatches.reduce(batteries, 0);
